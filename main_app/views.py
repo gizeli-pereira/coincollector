237fffe77ce0d437d8f2b9a2efa5297974202afa
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-04-21 10:44:09 -0500

    Finish lab part one

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+coins = [
+  {'value': 'Quarter', 'state': 'Maryland'},
+  {'value': 'Quarter', 'state': 'Texas'},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def coins_index(request):
+    return render(request, 'coins/index.html', {
+        'coins': coins
+    })
